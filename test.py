e406696eb9e7a05465c4432a3b8548d924bf0939
oystkva <oystkva@stud.ntnu.no>
2025-07-01 13:14:51 +0200

    Feat: added prompt alignment metric, which measures what percentage of instructions are satisfied

--- test.py
+++ test.py
@@ -1,23 +1,7 @@
 from utils import OllamaLocalModel, CustomLogger
 
-logger = CustomLogger()
-
-
-logger.info("initialzing evaluation model...")
-
-model = OllamaLocalModel(
-    model="gemma3n:e4b-it-q8_0",
-    base_url="https://beta.chat.nhn.no/ollama",
-    api_key_file=".api_key.txt"
-)
-
-logger.info("Model initialized successfully.")
-
-input_text = "Hello there"
-
-logger.info("Generating response for input: %s", input_text)
-actual_output = model.generate(input_text)
-logger.info("Response generated successfully.")
-
-print("Input:", input_text)
-print("Output:", actual_output)
+""" TODO:
+    Clean up print statements
+    vertical list of parameters test functions
+    Add parameters to test functions for seed, top_k, temp, etc.
+    """
