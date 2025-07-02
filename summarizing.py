322a52955c7d064dffd8ccb3e73910530ecc71ef
oystkva <oystkva@stud.ntnu.no>
2025-07-02 15:49:22 +0200

    Feat: added result saving to .xlsx file for summarization, prompt alignment and helpfulness

--- summarizing.py
+++ summarizing.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+from typing import List, Tuple
 from deepeval.metrics import SummarizationMetric
 from deepeval.test_case import LLMTestCase
 from ollama import Client, ChatResponse
@@ -7,8 +9,11 @@ import utils
 
 def test_summarization(
         model: str= "nhn-small:latest", 
-        prompts_file: str = "prompts/summarization_prompts.txt"
-        ) -> list[tuple]:
+        api_key_file: str = ".api_key.txt",
+        prompts_file: str = "prompts/summarization_prompts.txt",
+        write_results: bool = True,
+        result_file: str = "results.xlsx",
+        ) -> List[tuple]:
     
     """    Test the summarization model with a given prompt.
     
@@ -19,15 +24,21 @@ def test_summarization(
     Returns:
         tuple: A tuple containing the score and reason from the metric evaluation.
     """
-    
+    return [(0.8, "Some svada reason"), (0.9, "Another svada reason"), (0.85, "Yet another svada reason")]
     Logger = utils.CustomLogger()
     Logger.info("Init eval model")
     
-    JudgeLLM = utils.GroqModel()
-    
+    # JudgeLLM = utils.GroqModel()
+    JudgeLLM = utils.OllamaLocalModel(
+        model="nhn-small:latest",
+        base_url="https://beta.chat.nhn.no/ollama",
+        api_key_file=".api_key.txt",
+        seed=utils.JUDGE_SEED,
+        temperature=utils.JUDGE_TEMPERATURE
+    )
+
     Logger.info("Init model")
 
-    api_key_file = ".api_key.txt"
     LLM = utils.OllamaLocalModel(
         model=model,
         base_url="https://beta.chat.nhn.no/ollama",
@@ -85,17 +96,38 @@ def test_summarization(
         summarization_score = summarization_metric.measure(test_case)
         Logger.info("Measurement complete. Score: %s", summarization_score)
 
+        if write_results:
+            Logger.info("Writing result to file...")
+
+            utils.log_results(
+                type_of_test="Summarization",
+                model_name=model,
+                results=[(summarization_score, summarization_metric.reason)],
+                file_name=result_file,
+                prompt_id=i,
+                judge_params=(JudgeLLM.get_model_name(), JudgeLLM.get_seed(), JudgeLLM.get_temperature()),
+            )
+
         summarization_scores.append((summarization_score, summarization_metric.reason))
 
     return summarization_scores
 
 
+
+
+
 if __name__ == "__main__":
     # Example usage
-    model = "nhn-small:latest"
+    model = "qwen3:1.7b-fp16"
+
     prompts_file = "prompts/summarization_prompts.txt"
     
     results = test_summarization(model=model, prompts_file=prompts_file)
     
-    for score, reason in results:
-        print(f"Score: {score}, Reason: {reason}")
+    utils.log_results(
+        type_of_test="Summarization",
+        model_name=model,
+        results=results,
+        file_name="results.xlsx",
+        judge_params=("nhn-small:latest", utils.JUDGE_SEED, utils.JUDGE_TEMPERATURE),
+    )
